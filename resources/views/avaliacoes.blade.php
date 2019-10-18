d928acd90eef6c3b8eeed8f8e73567870809a63b
Matheushard <matheushard2013@gmail.com>
2019-10-18 17:49:25 -0300

    Iniciando Popup Exibir Avaliacao

--- resources/views/avaliacoes.blade.php
+++ resources/views/avaliacoes.blade.php
@@ -24,6 +24,43 @@
     <div class="card-footer">
         <a href="{{asset('/cidades/excel')}}" class="btn btn-sm btn-success" role="button">Gerar Excel</a>
     </div>
+</div>
+
+<!--*****************DIALOG RESUMO*****************-->
+
+<div class="modal" tabindex="-1" role="dialog" id="dlgAvaliacoes">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+  <form class="form-horizontal" id="formCidades">
+    <div class="modal-header">
+      <h5 class="modal-title">Resumo Avaliação</h5>
+    </div>
+    <div class="modal-body">
+      <div class="form-group">
+                <h1>Conteudo Teórico</h1><br>
+                <h3>Aplicação no Processo de trabalho:</h3>  
+              <div class="form-check">
+              <input class="form-check-input" type="checkbox"  value="radioSim_1" id="radioSim_1" disabled>
+              <label class="form-check-label">Proporcionou novos conhecimentos</label><br>
+              <input class="form-check-input" type="checkbox"  value="radioNao_1" id="radioNao_1" disabled>
+              <label class="form-check-label">Não me proporcionou conhecimento do que já possuia</label><br>
+          </div>
+
+          <div class="form-check">
+              <input class="form-check-input" type="checkbox"  value="radioSim_1" id="radioMuito_2" disabled>
+              <label class="form-check-label" id="muito()"></label><br>
+              <input class="form-check-input" type="checkbox"  value="radioNao_1" id="radiobom_2" disabled>
+              <label class="form-check-label">Bom</label><br>
+              <input class="form-check-input" type="checkbox"  value="radioSim_1" id="radioRegular_2" disabled>
+              <label class="form-check-label">Regular</label><br>
+              <input class="form-check-input" type="checkbox"  value="radioNao_1" id="radioRuim_2" disabled>
+              <label class="form-check-label">Ruim</label><br>
+          </div>
+      </div>
+
+     
+      
+      
 
 
 @endsection
@@ -46,7 +83,7 @@ $.ajaxSetup({
         "<td>"+a.descricao_cidade+ "/" + a.descricao_uf +"</td>"+
         "<td>"+tipoAgente(a.tipoAgente)+"</td>"+
         "<td>"+
-          '<button class="btn btn-xs btn-primary botoes" onclick="exibir('+a.id+')">Editar</button>'+
+          '<button class="btn btn-xs btn-primary botoes" onclick="exibir('+a.id+')">Resumo</button>'+
           "</td>"+
         "</tr>";
 
@@ -56,13 +93,63 @@ $.ajaxSetup({
 
     function exibir(id){
         
-        $.getJSON('/api/avaliacoes/'+id , function(data){
-          console.log(data);
+        $.getJSON('/api/avaliacoes/'+id, function(data){
+        
+                  
+        checkedFalse();
+          
+         if(data[0].radioSim_1 == 1){$("#radioSim_1").attr('checked', true);}
+         if(data[0].radioNao_1 == 1){$("#radioNao_1").attr('checked', true);}
+        
+         if(data[0].radioMuito_2 == 1){$("#radioMuito_2").attr('checked', true);}
+         if(data[0].radiobom_2 == 1){$("#radiobom_2").attr('checked', true);}
+         if(data[0].radioRegular_2 == 1){$("#radioRegular_2").attr('checked', true);}
+         if(data[0].radioRuim_2 == 1){$("#radioRuim_2").attr('checked', true);}
+       
+         if(data[0].radioSeguro_3 == 1){$("#radioSeguro_3").attr('checked', true);}
+         if(data[0].radioPoucoSeguro_3 == 1){$("#radioPoucoSeguro_3").attr('checked', true);}
+         if(data[0].radioInseguro_3 == 1){$("#radioInseguro_3").attr('checked', true);}
 
-          /*$('#id').val(data.id);
-          $('#descricao_cidade').val(data.descricao_cidade);
-          $('#uf_id').val(data.uf_id);
-          $('#dlgCidades').modal('show');*/
+         if(data[0].radioExcessiva_4 == 1){$("#radioExcessiva_4").attr('checked', true);}
+         if(data[0].radioRazoavel_4 == 1){$("#radioRazoavel_4").attr('checked', true);}
+         if(data[0].radioInsuficiente_4 == 1){$("#radioInsuficiente_4").attr('checked', true);}
+           
+         if(data[0].radioMuito_5 == 1){$("#radioMuito_5").attr('checked', true);}
+         if(data[0].radiobom_5 == 1){$("#radiobom_5").attr('checked', true);}
+         if(data[0].radioRegular_5 == 1){$("#radioRegular_5").attr('checked', true);}
+         if(data[0].radioRuim_5 == 1){$("#radioRuim_5").attr('checked', true);}
+
+         if(data[0].radioMuito_6 == 1){$("#radioMuito_6").attr('checked', true);}
+         if(data[0].radiobom_6 == 1){$("#radiobom_6").attr('checked', true);}
+         if(data[0].radioRegular_6 == 1){$("#radioRegular_6").attr('checked', true);}
+         if(data[0].radioRuim_6 == 1){$("#radioRuim_6").attr('checked', true);}
+
+         if(data[0].radioMuito_7 == 1){$("#radioMuito_7").attr('checked', true);}
+         if(data[0].radiobom_7 == 1){$("#radiobom_7").attr('checked', true);}
+         if(data[0].radioRegular_7 == 1){$("#radioRegular_7").attr('checked', true);}
+         if(data[0].radioRuim_7 == 1){$("#radioRuim_7").attr('checked', true);}
+      
+         if(data[0].radioMuito_8 == 1){$("#radioMuito_8").attr('checked', true);}
+         if(data[0].radiobom_8 == 1){$("#radiobom_8").attr('checked', true);}
+         if(data[0].radioRegular_8 == 1){$("#radioRegular_8").attr('checked', true);}
+         if(data[0].radioRuim_8 == 1){$("#radioRuim_8").attr('checked', true);}
+
+
+         if(data[0].radioMuito_9 == 1){$("#radioMuito_9").attr('checked', true);}
+         if(data[0].radiobom_9 == 1){$("#radiobom_9").attr('checked', true);}
+         if(data[0].radioRegular_9 == 1){$("#radioRegular_9").attr('checked', true);}
+         if(data[0].radioRuim_9 == 1){$("#radioRuim_9").attr('checked', true);}
+        
+         if(data[0].radioMuito_10 == 1){$("#radioMuito_10").attr('checked', true);}
+         if(data[0].radiobom_10 == 1){$("#radiobom_10").attr('checked', true);}
+         if(data[0].radioRegular_10 == 1){$("#radioRegular_10").attr('checked', true);}
+         if(data[0].radioRuim_10 == 1){$("#radioRuim_10").attr('checked', true);}
+        
+         if(data[0].descricao == 1){$("#descricao").attr('checked', true);}
+         if(data[0].nome_agente == 1){$("#nome_agente").attr('checked', true);}
+         if(data[0].tipo_agente == 1){$("#tipo_agente").attr('checked', true);}
+
+        $('#dlgAvaliacoes').modal('show');
         });
        }
 
@@ -70,7 +157,7 @@ $.ajaxSetup({
       
        function listarAvaliacoes (){
 
-      $.getJSON('/api/avaliacoes' , function(avaliacoes){
+      $.getJSON('/api/avaliacoes', function(avaliacoes){
 
         for( i = 0; i < avaliacoes.length; i++){
      
@@ -82,6 +169,61 @@ $.ajaxSetup({
 
  /****************UTILS****************/
 
+function checkedFalse(){
+          
+          $("#radioSim_1").attr('checked', false);
+          $("#radioNao_1").attr('checked', false);
+
+          $("#radioMuito_2").attr('checked', false);
+          $("#radiobom_2").attr('checked', false);
+          $("#radioRegular_2").attr('checked', false);
+          $("#radioRuim_2").attr('checked', false);
+       
+         /* $("#radioSeguro_3").attr('checked', false);
+          $("#radioPoucoSeguro_3").attr('checked', false);
+          $("#radioInseguro_3").attr('checked', false);
+
+          $("#radioExcessiva_4").attr('checked', false);
+          $("#radioRazoavel_4").attr('checked', false);
+          $("#radioInsuficiente_4").attr('checked', false);
+           
+          $("#radioMuito_5").attr('checked', false);
+          $("#radiobom_5").attr('checked', false);
+          $("#radioRegular_5").attr('checked', false);
+          $("#radioRuim_5").attr('checked', false);
+
+          $("#radioMuito_6").attr('checked', false);
+          $("#radiobom_6").attr('checked', false);
+          $("#radioRegular_6").attr('checked', false);
+          $("#radioRuim_6").attr('checked', false);
+
+          $("#radioMuito_7").attr('checked', false);
+          $("#radiobom_7").attr('checked', false);}
+          $("#radioRegular_7").attr('checked', false);
+          $("#radioRuim_7").attr('checked', false);
+      
+          $("#radioMuito_8").attr('checked', false);
+          $("#radiobom_8").attr('checked', false);
+          $("#radioRegular_8").attr('checked', false);
+          $("#radioRuim_8").attr('checked', false);
+
+
+          $("#radioMuito_9").attr('checked', false);
+          $("#radiobom_9").attr('checked', false);
+          $("#radioRegular_9").attr('checked', false);
+          $("#radioRuim_9").attr('checked', false);
+
+          $("#radioMuito_10").attr('checked', false);
+          $("#radiobom_10").attr('checked', false);
+          $("#radioRegular_10").attr('checked', false);
+          $("#radioRuim_10").attr('checked', false);
+        
+          $("#descricao").attr('checked', false);
+          $("#nome_agente").attr('checked', false);
+          $("#tipo_agente").attr('checked', false);*/
+
+        }
+
  function tipoAgente(tipo){
     
     if(tipo == 1){
@@ -89,6 +231,19 @@ $.ajaxSetup({
     }else{
       return "ACE";
     }
+   }
+
+   function muito(){
+     return "Muito Bom";
+   }
+   function bom(){
+     return "Bom";
+   }
+   function regular(){
+     return "Regular";
+   }
+   function ruim(){
+     return "Ruim";
    }
 
 /*********AUTO LOAD**********/
